cf313788315a04300474fa425b178dc8bbd2d83e
sansovet <sansan503451@gmail.com>
2025-04-07 18:19:11 +0500

    фы

---
+++ applications.py
@@ -0,0 +1,19 @@
+import numpy as np
+import pandas as pdd
+
+from engine.environment import Environment
+from engine.brains import *
+
+
+from visualization.visualization import Visualization
+from visualization.parameters import *
+
+
+# В этом файле я планирую написать разные классыи приложений потому-что иногда мне надо запускать open-ended, 
+# иногда Genetic algorithm с каким-то определенным Selection, в другой момент GA с другим Selection
+
+
+
+
+class OpenEndedApp()
+    pass
